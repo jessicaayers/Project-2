bb1f04157d9a4dd4d4acd8477b839de7970086e3
jessicaayers <134238613+jessicaayers@users.noreply.github.com>
2023-07-09 08:23:33 -0400

    Update WorldAnalysis.R
    R Markdown syntax to normal r script

--- WorldAnalysis.R
+++ WorldAnalysis.R
@@ -1,22 +1,14 @@
-```{r packages, message = FALSE}
 library(tidyverse)
 library(rmarkdown)
 library(caret)
 library(corrplot)
 library(gbm)
 library(randomForest)
-```
 
-```{r setup, include=FALSE}
 knitr::opts_chunk$set(fig.path="./images/")
 knitr::opts_chunk$set(echo = TRUE)
-```
 
-```{r data, message = FALSE,warning=FALSE}
-#import data set
-#my data set is stored in the folder above where I have the project 2 folder
 onpdata <- read_csv("../OnlineNewsPopularity/OnlineNewsPopularity.csv")
-#create new variable to represent type of channel to use as parameters
 for(i in 1:nrow(onpdata)){
   if(onpdata$data_channel_is_lifestyle[i] == 1){
     onpdata$channel[i] <- "Lifestyle"
@@ -37,18 +29,14 @@ for(i in 1:nrow(onpdata)){
     onpdata$channel[i] <- "World"
   }
 }
-```
 
-```{r automation}
 channelIDs <- unique(onpdata$channel)
 output_file <- paste0(channelIDs, "Analysis.md")
 params <- lapply(channelIDs, FUN = function(x){list(channel=x)})
 analysis <- tibble(output_file, params)
-```
 
-```{r render, eval = FALSE}
 for(i in 1:6){
 onpdata_subset <- subset(onpdata, channel == params[[i]])
 render(input = "WorldAnalysis.Rmd", output_file = output_file[i], params = params[i])
 }
-```
+
